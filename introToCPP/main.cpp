95f7cf889a016fe475abeb463b48771458eed535
Asigan <Zacharycgrippo@gmail.com>
2021-11-29 16:55:27 -0600

    Initial setup

---
+++ introToCPP/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include "Entity.h"
+void changeString(char string[64])
+	{
+	char test[64] = "test";
+	}
+
+
+int main()
+{
+	// Icon, health, attack, defense.
+	Entity wompus ('W', 50000000, 5, -20);
+
+	return 0;
+}
